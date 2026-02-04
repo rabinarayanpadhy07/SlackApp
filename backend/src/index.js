8b2a12c0c94908a1922119200354f11a275a1ab5
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-02-04 20:56:08 +0530

    added the basic routing layer

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
