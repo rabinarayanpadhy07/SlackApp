0cc04dce0ab20b2ac5157d09fe95b9518f421df4
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-02-04 19:05:45 +0530

    Update The latest node pacakge

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,20 +1,21 @@
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+
+export default defineConfig([
   {
-    languageOptions: {
-      globals: globals.node
-    },
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: [js.configs.recommended],
     plugins: {
       'simple-import-sort': simpleImportSort
     },
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error'
+    },
+    languageOptions: {
+      globals: globals.node
     }
-  },
-
-  pluginJs.configs.recommended
-];
+  }
+]);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-import connectDB from './config/dbConfig.js';
-import { PORT } from './config/serverConfig.js';
+import { PORT } from './config/ServerConfig.js';
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
@@ -15,5 +11,4 @@ app.get('/ping', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB();
 });
