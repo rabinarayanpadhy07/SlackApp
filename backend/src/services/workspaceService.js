9c08e6fce68fee9c2c843a88ae15fdf16f5459b3
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-02-22 20:21:35 +0530

    Aded get channel

--- backend/src/services/workspaceService.js
+++ backend/src/services/workspaceService.js
@@ -19,7 +19,7 @@ const isUserAdminOfWorkspace = (workspace, userId) => {
   return response;
 };
 
-const isUserMemberOfWorkspace = (workspace, userId) => {
+export const isUserMemberOfWorkspace = (workspace, userId) => {
   return workspace.members.find(
     (member) => member.memberId.toString() === userId
   );
