a708fb426441afb090aeeec387cf09b8371d55fb
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-03-04 18:25:06 +0530

    Added predesignedlogics

--- backend/src/routes/v1/messages.js
+++ backend/src/routes/v1/messages.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { getMessages } from '../../controllers/messageController.js';
+import { getMessages, getPresignedUrlFromAWS } from '../../controllers/messageController.js';
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+router.get('/pre-signed-url', isAuthenticated, getPresignedUrlFromAWS);
 router.get('/:channelId', isAuthenticated, getMessages);
+
 
 export default router;
