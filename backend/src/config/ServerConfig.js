242b8f33e379b2d30e6f756a2ee3cf62faadf98a
rabinarayanpadhy07 <132149572+rabinarayanpadhy07@users.noreply.github.com>
2026-03-04 20:25:15 +0530

    Integreted payment gateways

--- backend/src/config/ServerConfig.js
+++ backend/src/config/ServerConfig.js
@@ -27,7 +27,6 @@ export const APP_LINK = process.env.APP_LINK || 'http://localhost:3000';
 export const ENABLE_EMAIL_VERIFICATION =
   process.env.ENABLE_EMAIL_VERIFICATION || false;
 
-
 export const AWS_REGION = process.env.AWS_REGION;
 
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
@@ -35,3 +34,11 @@ export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+
+export const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
+
+export const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
+
+export const CURRENCY = process.env.CURRENCY || 'INR';
+
+export const RECEIPT_SECRET = process.env.RECEIPT_SECRET || 'receipt_1103';
--- backend/src/routes/v1/v1Router.js
+++ backend/src/routes/v1/v1Router.js
@@ -3,6 +3,7 @@ import express from 'express';
 import channelRouter from './channel.js';
 import memberRouter from './members.js';
 import messageRouter from './messages.js';
+import paymentRouter from './payment.js';
 import userRouter from './users.js';
 import workspaceRouter from './workspaces.js';
 const router = express.Router();
@@ -16,5 +17,7 @@ router.use('/channels', channelRouter);
 router.use('/members', memberRouter);
 
 router.use('/messages', messageRouter);
+
+router.use('/payments', paymentRouter);
 
 export default router;
